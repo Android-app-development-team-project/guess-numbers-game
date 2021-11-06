a54fbba8a52012d3117d42f02d022a1f53b95e8c
jgy4419 <jgy_98@naver.com>
2021-11-07 00:17:36 +0900

    11/7

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -16,56 +16,92 @@ public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
     ButtonClick buttonClick;
     Button btnAdd, btnSub, btnMul, btnDiv; // +, -, *, / 버튼
-    Integer[] numBtn = {R.id.num1,R.id.num2,R.id.num3,R.id.num4,R.id.num5,R.id.num6,R.id.num7,R.id.num8,R.id.num9,R.id.num10,R.id.num11,R.id.num12}; // 숫자 버튼
-    int num1;
-    int num2; // 클릭되어진 숫자.
-    TextView result;
-    TextView resNum1;
-    TextView resNum2; // 결과 text, 첫 번째 누른 숫자 text, 두 번째 누른 숫자 text
+    Integer[] numBtn = {R.id.num1,R.id.num2,R.id.num3,R.id.num4,R.id.num5,R.id.num6,R.id.num7,R.id.num8,R.id.num9,R.id.num10, R.id.num11, R.id.num12}; // 숫자 버튼
+//    Button numBtn1;
+    Button[] numButtons = new Button[12];
+    String num1;
+    String num2; // 클릭되어진 숫자.
+    TextView resultText;
+    Integer[] res = {R.id.first, R.id.second};
+    TextView[] resNum = new TextView[2];
+    Integer result;
+    EditText edit1, edit2;
+    String  res1Input, res2Input; // 실제 결과값이 들어갈 부분.
+    int watchResult;
+    int i, j;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("숫자 맞추기 게임");
 
-        // 연산 기능을 해주는 로직. (+, -, *, /)
-
         // 1. +, -, *, - 을 지정해주기.
-        resNum1 = (TextView)findViewById(R.id.first);
-        resNum2 = (TextView)findViewById(R.id.second);
         btnAdd = (Button)findViewById(R.id.plus);
         btnSub = (Button)findViewById(R.id.minus);
         btnMul = (Button)findViewById(R.id.multiply);
         btnDiv = (Button)findViewById(R.id.division);
-
-        result = (TextView)findViewById(R.id.result);
-
-        // + 버튼을 눌렀을 때
+        resultText = (TextView)findViewById(R.id.result);
+        // 숫자 버튼들을 지정, 숫자 버튼들을 눌렀을 때 resNum1, resNum2에 누른 버튼 값으로 변경시켜주기.
+        // 손보기..
+        for (i = 0; i <numBtn.length; i++) {
+            numButtons[i] = (Button) findViewById(numBtn[i]);
+        }
+        for (i = 0; i <numBtn.length; i++) {
+            final int index; //꼭 필요함
+            index = i;
+            for(j = 0; j < res.length; j++){
+                resNum[j] = (TextView) findViewById(res[j]);
+            }
+            for(j = 0; j < res.length; j++){
+                final int index2;
+                index2 = j;
+                numButtons[index].setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view) {
+                        resNum[0].setText(numButtons[index].getText().toString());
+                        resNum[1].setText(numButtons[index].getText().toString());
+                    }
+                });
+            }
+        }
+        // 숫자 입력 후 덧셈을 눌렀을 때
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                num1 = Integer.parseInt(resNum1.getText().toString());
-                num2 = Integer.parseInt(resNum2.getText().toString());
-//                result = num1 + num2;
-                result.setText("계산결과 : ");
+                res1Input = resNum[0].getText().toString();
+                res2Input = resNum[1].getText().toString();
+                watchResult = Integer.parseInt(res1Input) + Integer.parseInt(res2Input);
+                resultText.setText(String.valueOf(watchResult));
             }
         });
-//        btnAdd.setOnTouchListener(new View.OnTouchListener(){
-//            public boolean onTouch(View arg0, MotionEvent arg1){
-//                num1 = Integer.parseInt(resNum1.getText().toString());
-//                num2 = Integer.parseInt(resNum2.getText().toString());
-////                result = num1 + num2;
-//                result.setText("계산결과 : " + result.toString());
-//                return false;
-//            }
-//        });
-
-        // mainActivity에 숫자판이 나오도록 설정.
-        fragmentManager = getSupportFragmentManager();
-        buttonClick = new ButtonClick();
-
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.add(R.id.buttonClick, buttonClick, "btnClick");
-        ft.commitAllowingStateLoss();
+        // 숫자 입력 후 뺄셈을 눌렀을 때
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                res1Input = resNum[0].getText().toString();
+                res2Input = resNum[1].getText().toString();
+                watchResult = Integer.parseInt(res1Input) - Integer.parseInt(res2Input);
+                resultText.setText(String.valueOf(watchResult));
+            }
+        });
+        // 숫자 입력 후 곱셈을 눌렀을 때
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                res1Input = resNum[0].getText().toString();
+                res2Input = resNum[1].getText().toString();
+                watchResult = Integer.parseInt(res1Input) * Integer.parseInt(res2Input);
+                resultText.setText(String.valueOf(watchResult));
+            }
+        });
+        // 숫자 입력 후 나눗셈을 눌렀을 때
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                res1Input = resNum[0].getText().toString();
+                res2Input = resNum[1].getText().toString();
+                watchResult = Integer.parseInt(res1Input) / Integer.parseInt(res2Input);
+                resultText.setText(String.valueOf(watchResult));
+            }
+        });
     }
 }
