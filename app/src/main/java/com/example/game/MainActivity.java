676173d780ead2bc0eee7e36e2a8825753980c86
jgy4419 <jgy_98@naver.com>
2021-11-07 20:49:35 +0900

    11/7

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -16,15 +16,15 @@ public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
     ButtonClick buttonClick;
     Button btnAdd, btnSub, btnMul, btnDiv; // +, -, *, / 버튼
+    Button submit;
     Integer[] numBtn = {R.id.num1,R.id.num2,R.id.num3,R.id.num4,R.id.num5,R.id.num6,R.id.num7,R.id.num8,R.id.num9,R.id.num10, R.id.num11, R.id.num12}; // 숫자 버튼
-//    Button numBtn1;
     Button[] numButtons = new Button[12];
     String num1;
     String num2; // 클릭되어진 숫자.
     TextView resultText;
     Integer[] res = {R.id.first, R.id.second};
     TextView[] resNum = new TextView[2];
-    Integer result;
+    TextView important;
     EditText edit1, edit2;
     String  res1Input, res2Input; // 실제 결과값이 들어갈 부분.
     int watchResult;
@@ -52,17 +52,22 @@ public class MainActivity extends AppCompatActivity {
             for(j = 0; j < res.length; j++){
                 resNum[j] = (TextView) findViewById(res[j]);
             }
-            for(j = 0; j < res.length; j++){
-                final int index2;
-                index2 = j;
-                numButtons[index].setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
+            numButtons[index].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    for(j = 0; j < res.length; j++){
+                        final int index2;
                         resNum[0].setText(numButtons[index].getText().toString());
-                        resNum[1].setText(numButtons[index].getText().toString());
+                        numButtons[index].setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                resNum[1].setText(numButtons[index].getText().toString());
+                            }
+                        });
                     }
-                });
-            }
+                }
+            });
         }
+
         // 숫자 입력 후 덧셈을 눌렀을 때
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,6 +106,30 @@ public class MainActivity extends AppCompatActivity {
                 res2Input = resNum[1].getText().toString();
                 watchResult = Integer.parseInt(res1Input) / Integer.parseInt(res2Input);
                 resultText.setText(String.valueOf(watchResult));
+            }
+        });
+        // 랜덤 값 가져와서 result에 지정하기.
+        int random = (int) (Math.random() * 20) + 1; // 1 ~ 20까지의 랜덤 값 지정.
+        important = (TextView)findViewById(R.id.important);
+        important.setText(String.valueOf(random));
+
+        submit = (Button) findViewById(R.id.submit);
+        // 제출을 클릭했을 때 문제랑 결과같 같으면 true, 다르면 false 출력 (임시로 Toast 출력)
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // String 으로 지정.
+//                res1Input = resNum[0].getText().toString();
+//                res2Input = resNum[1].getText().toString();
+//                watchResult = Integer.parseInt(res1Input) * Integer.parseInt(res2Input);
+                String resultInt = resultText.getText().toString();
+                String importantInt = important.getText().toString();
+                if(Integer.parseInt(resultInt) == Integer.parseInt(importantInt)){
+                    Toast.makeText(getApplicationContext(), "성공입니다!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "실패입니다..", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
